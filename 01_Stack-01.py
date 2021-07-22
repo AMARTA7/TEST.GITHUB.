d98131d6a435ae81c6324728c1ac0d2e8d9adf4f
AMARTA7 <amartakundu247@gmail.com>
2021-07-22 12:07:33 +0530

    stack

--- 01_Stack-01.py
+++ 01_Stack-01.py
@@ -4,6 +4,16 @@ Last In First Out (LIFO). This means the last element inserted inside the
 stack is removed first.
 -------------------------------------------------------------------------------------------------
 
+* Basic Operations of Stack:
+    There are some basic operations that allow us to perform different actions on a stack:
+
+        Push: Add an element to the top of a stack
+        Pop: Remove an element from the top of a stack
+        IsEmpty: Check if the stack is empty
+        IsFull: Check if the stack is full
+        Peek: Get the value of the top element without removing it.
+
+
 *** Working of Stack Data Structure:
 
     1. A pointer called 'TOP' is used to keep track of the top element in the stack.
